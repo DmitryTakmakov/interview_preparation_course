ee927c8539685692ba54d76f42b05ea852ea6722
Dmitry Takmakov <dj-dat@yandex.ru>
2021-12-04 03:21:35 +0500

    task 5 done

--- lesson_4-6/interview_course/catalogapp/models.py
+++ lesson_4-6/interview_course/catalogapp/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=64,
+                                     verbose_name='category name')
+
+    def __str__(self):
+        return self.category_name
+
+
 class ProductItem(models.Model):
     product_name = models.CharField(max_length=128,
                                     verbose_name='product name')
+    product_category = models.ManyToManyField(ProductCategory)
     added = models.DateTimeField(auto_now_add=True, verbose_name='date added')
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0,
                                 verbose_name='price')
