8f1733f0b8b91031eac8a3d5f766abc9e00897da
Dmitry Takmakov <dj-dat@yandex.ru>
2021-12-09 03:30:28 +0500

    all tasks done

---
+++ lesson_4-6/interview_course/settings/settings/local.py
@@ -0,0 +1,5 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-+n$ljmeacnf6j^e@a9d(__*6mpa3*pn29#$deq5ed#rm0=n@$s'
+DEBUG = True
